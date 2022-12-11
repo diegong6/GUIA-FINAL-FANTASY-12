2e68044b1fdaf0225b8462726463f1799b47cfd7
Diego Villegas <diegon.villegas@gmail.com>
2022-12-11 15:36:40 -0300

    Se modifica inicio y se cambian algunas estructuras en js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const intro1 = document.querySelector('.intro1');
-document.addEventListener('DOMContentLoaded', (e)=>{
-    setTimeout(()=>{
+document.addEventListener('DOMContentLoaded', (e) => {
+    setTimeout(() => {
         intro1.classList.add('display-none');
-    }, 2000);
+    }, 3000);
 })
+
+document.oncontextmenu = function () { return false }
